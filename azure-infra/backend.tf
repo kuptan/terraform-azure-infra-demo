1bfc1a6e9b349ba41df05352f79429dc20e41040
ibraheem saady <ibraheem@ibraheems-MacBook-Pro.local>
2021-01-07 13:03:26 +0100

    added cirlce ci config

--- azure-infra/backend.tf
+++ azure-infra/backend.tf
@@ -1,10 +1,6 @@
-terraform {
-  backend "remote" {
-    hostname = "app.terraform.io"
-    organization = "kubechamp"
-
-    workspaces {
-      name = "azure-aks-infra"
-    }
-  }
+backend "azurerm" {
+  resource_group_name  = "tf-storage"
+  storage_account_name = "azakstfstorage"
+  container_name       = "tfstate"
+  key                  = "infra.demo.terraform.tfstate"
 }
