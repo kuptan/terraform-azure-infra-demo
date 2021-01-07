7934e1cff3ffc81518f1a3f68324e83656a4a59b
ibraheem saady <ibraheem@ibraheems-MacBook-Pro.local>
2021-01-07 13:07:35 +0100

    fix azure infra backend section

--- azure-infra/backend.tf
+++ azure-infra/backend.tf
@@ -1,6 +1,8 @@
-backend "azurerm" {
-  resource_group_name  = "tf-storage"
-  storage_account_name = "azakstfstorage"
-  container_name       = "tfstate"
-  key                  = "infra.demo.terraform.tfstate"
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "tf-storage"
+    storage_account_name = "azakstfstorage"
+    container_name       = "tfstate"
+    key                  = "infra.demo.terraform.tfstate"
+  }
 }
